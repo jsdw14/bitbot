52d9511020c0e7d43dd239152100efe0a95cb32f
jsdw14 <jsdw@icloud.com>
2023-05-08 20:55:19 +1000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    bitbot.ledRainbow()
+    while (line % 2 == 0) {
+        while (bitbot.readLine(BBLineSensor.Left) == 0) {
+            bitbot.move(BBMotor.Left, BBDirection.Forward, 20)
+            bitbot.move(BBMotor.Right, BBDirection.Forward, 5)
+        }
+        while (bitbot.readLine(BBLineSensor.Right) == 0) {
+            bitbot.move(BBMotor.Right, BBDirection.Forward, 20)
+            bitbot.move(BBMotor.Left, BBDirection.Forward, 5)
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    line += 1
+})
+input.onButtonPressed(Button.B, function () {
+    line += 1
+    bitbot.setLedColor(0xFF0000)
+    bitbot.goms(BBDirection.Reverse, 40, 400)
+})
+let line = 0
+bitbot.select_model(BBModel.XL)
+line = 0
+bitbot.ledBrightness(20)
+bitbot.setLedColor(0xFF0000)
 basic.forever(function () {
-	
+    basic.showNumber(line)
 })
